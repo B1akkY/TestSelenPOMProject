dcf52714e4c52b59d6dc156682dec6ba7d075d59
B1ackY <756478@gmail.com>
2022-09-16 01:18:30 +0300

    Update fixture (firefox setup died the moment I was working with it)

--- conftest.py
+++ conftest.py
@@ -1,29 +1,30 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
+import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser_name", action="store", default="chrome",
-                     help="Choose browser: chrome or firefox")
-    parser.addoption("--language", action="store", default="en",
-                     help="Choose language: ru, en, fr, etc")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help='Choose browser: chrome or firefox')
+    parser.addoption('--language', action='store', default="en-gb",
+                     help='Choose language: es, en-gb, ru or fr')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
     browser = None
     if browser_name == "chrome":
         options = Options()
-        options.add_experimental_option("prefs", {"intl.accept_language": user_language})
+        options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
         browser = webdriver.Chrome(options=options)
         browser.maximize_window()
     elif browser_name == "firefox":
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_language", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
+        options = Options()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
         browser.maximize_window()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
