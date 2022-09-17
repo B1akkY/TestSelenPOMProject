28c337b6f0380ea09560bd69613270617dc86dcc
B1ackY <756478@gmail.com>
2022-09-18 01:01:58 +0300

    Update locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,6 @@ class LoginPageLocators():
 class ProductPageLocators():
     ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     PROMO_PRICE = (By.CSS_SELECTOR, "p.price_color:nth-child(2)")
+    BASKET_TOTAL = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-info.fade.in p:nth-child(1) strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main h1")
+    PRODUCT_NAME_ADDED = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
