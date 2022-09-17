9b21551585e30b67eb3e0b1efcade256cfd95247
B1ackY <756478@gmail.com>
2022-09-17 23:46:33 +0300

    Add more locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PROMO_PRICE = (By.CSS_SELECTOR, "p.price_color:nth-child(2)")
