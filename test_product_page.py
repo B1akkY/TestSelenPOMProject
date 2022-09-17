f731e8aeba316ff1a3d5b3d818bd13a467621e65
B1ackY <756478@gmail.com>
2022-09-18 01:01:22 +0300

    Update test-cases

--- test_product_page.py
+++ test_product_page.py
@@ -1,23 +1,43 @@
+import pytest
 from .pages.product_page import ProductPage
 
-link = "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
-
-def test_guest_can_add_to_basket(browser):
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+    "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"],)
+def test_guest_can_add_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.click_add_to_basket_btn()
     page.solve_quiz_and_get_code()
 
 
-def test_guest_should_see_basket_btn(browser):
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+    "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"],)
+def test_guest_should_see_basket_btn(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_btn()
 
 
-def test_guest_should_have_promo_price(browser):
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+    "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"],)
+def test_guest_should_have_promo_price_equals_total_added(browser, link):
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_promo_price()
+    page.click_add_to_basket_btn()
+    page.solve_quiz_and_get_code()
+    page.should_be_promo_price_equals_total()
 
+
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+    "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"],)
+def test_guest_should_have_expected_product_to_be_added_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_add_to_basket_btn()
+    page.solve_quiz_and_get_code()
+    page.should_be_product_name_added_equals_product_name()
