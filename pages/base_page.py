50702d6df97291d3d77f9ee23aef51127193d75f
B1ackY <756478@gmail.com>
2022-09-19 02:21:04 +0300

    Add methods and locators for basket

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import MainPageLocators
 from .locators import BasePageLocators
 import math
 import pyperclip
@@ -61,3 +62,7 @@ class BasePage:
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented!"
+
+    def go_to_basket_page(self):
+        basket = self.browser.find_element(*MainPageLocators.BASKET_BTN_NAVBAR)
+        basket.click()
