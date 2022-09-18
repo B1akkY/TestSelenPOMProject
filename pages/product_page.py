32fdf6d137e3fe0d9d91f8462ca92abf133fd930
B1ackY <756478@gmail.com>
2022-09-18 19:19:35 +0300

    Update product_page.py methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -20,3 +20,11 @@ class ProductPage(BasePage):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         product_name_added = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ADDED).text
         assert product_name == product_name_added, "Wrong product added to basket!"
+
+    def should_not_be_success_message(self):
+        assert not self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_success_message_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message has not disappeared, but it should"
