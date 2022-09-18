42e724d078687d40d26ec4a3b78e2c6e9ddf7426
B1ackY <756478@gmail.com>
2022-09-18 19:19:09 +0300

    Update test-cases

--- test_product_page.py
+++ test_product_page.py
@@ -16,28 +16,60 @@ from .pages.product_page import ProductPage
                  marks=pytest.mark.xfail),
     "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
     "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-class TestProductPage():
-    def test_guest_can_add_product_to_basket(self, browser, link):
-        page = ProductPage(browser, link)
-        page.open()
-        page.click_add_to_basket_btn()
-        page.solve_quiz_and_get_code()
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_add_to_basket_btn()
+    page.solve_quiz_and_get_code()
+    page.should_be_product_name_added_equals_product_name()
 
-    def test_guest_should_see_basket_btn(self, browser, link):
-        page = ProductPage(browser, link)
-        page.open()
-        page.should_be_add_to_basket_btn()
 
-    def test_guest_should_have_promo_price_equals_total_added(self, browser, link):
-        page = ProductPage(browser, link)
-        page.open()
-        page.click_add_to_basket_btn()
-        page.solve_quiz_and_get_code()
-        page.should_be_promo_price_equals_total()
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+    "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"])
+def test_guest_should_see_basket_btn(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket_btn()
 
-    def test_guest_should_have_expected_product_to_be_added_to_basket(self, browser, link):
-        page = ProductPage(browser, link)
-        page.open()
-        page.click_add_to_basket_btn()
-        page.solve_quiz_and_get_code()
-        page.should_be_product_name_added_equals_product_name()
+
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+    "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"])
+def test_guest_should_have_promo_price_equals_total_added(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_add_to_basket_btn()
+    page.solve_quiz_and_get_code()
+    page.should_be_promo_price_equals_total()
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209",
+    "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207"])
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_add_to_basket_btn()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209",
+    "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207"])
+def test_guest_cant_see_success_message(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("link", [
+    "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209",
+    "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207"])
+def test_message_disappeared_after_adding_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_add_to_basket_btn()
+    page.should_be_success_message_disappeared()
