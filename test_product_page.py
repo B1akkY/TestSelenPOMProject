d595f9f941a1f0ba7620e29e335133c83018f715
B1ackY <756478@gmail.com>
2022-09-19 02:22:02 +0300

    Add test-cases

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 
 
 @pytest.mark.parametrize("link", [
@@ -85,7 +86,6 @@ def test_guest_should_see_login_link_on_product_page(browser, link):
     page.should_be_login_link()
 
 
-
 @pytest.mark.parametrize("link", [
     "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209",
     "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207",
@@ -94,3 +94,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
