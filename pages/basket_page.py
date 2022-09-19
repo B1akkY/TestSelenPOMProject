a473a38cf06992da94539a239ec8098c0e04f964
B1ackY <756478@gmail.com>
2022-09-19 08:47:26 +0300

    Add multi-language function support for basket functions

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,6 +1,30 @@
 from .base_page import BasePage
 from .locators import BasketPageLocators
 
+languages = {
+    "ar": "سلة التسوق فارغة",
+    "ca": "La seva cistella està buida",
+    "cs": "Váš košík je prázdný",
+    "da": "Din indkøbskurv er tom",
+    "de": "Ihr Warenkorb ist leer",
+    "en": "Your basket is empty",
+    "el": "Το καλάθι σας είναι άδειο",
+    "es": "Tu carrito esta vacío",
+    "fi": "Korisi on tyhjä",
+    "fr": "Votre panier est vide",
+    "it": "Il tuo carrello è vuoto",
+    "ko": "장바구니가 비었습니다",
+    "nl": "Je winkelmand is leeg",
+    "pl": "Twój koszyk jest pusty",
+    "pt": "O carrinho está vazio",
+    "pt-br": "Sua cesta está vazia",
+    "ro": "Cosul tau este gol",
+    "ru": "Ваша корзина пуста",
+    "sk": "Váš košík je prázdny",
+    "uk": "Ваш кошик пустий",
+    "zh-cn": "Your basket is empty",
+}
+
 
 class BasketPage(BasePage):
     def should_be_empty_basket(self):
@@ -8,7 +32,11 @@ class BasketPage(BasePage):
         self.should_be_basket_is_empty_notification()
 
     def should_be_basket_is_empty(self):
-        assert not self.is_element_present(*BasketPageLocators.BUY_BTN)
+        assert not self.is_element_present(*BasketPageLocators.BUY_BTN), \
+            "There is a product in the basket, although there should not be one"
 
     def should_be_basket_is_empty_notification(self):
-        assert self.is_element_present(*BasketPageLocators.BASKET_IS_EMPTY_NOTIFICATION)
+        text = self.browser.find_element(*BasketPageLocators.BASKET_IS_EMPTY_NOTIFICATION).text
+        text_lst = text.split('.')
+        empty_text = text_lst[0]
+        assert empty_text in languages.values(), "There is no empty basket text presented"
